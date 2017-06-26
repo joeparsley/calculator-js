8ef13058f42fd1f9a7bae4aa82dfe51c6e19d393
Joe Parsley and Rose Sponseller <joe@>
2017-06-26 13:17:20 -0700

    Add conditional statement to connect our Calculator constructor to the front end

--- build/js/export.js
+++ build/js/export.js
@@ -35,15 +35,22 @@ $(document).ready(function() {
   $('#calc').submit(function(event) {
     event.preventDefault();
 
-
     var firstNumber = parseInt($('#input1').val());
     var secondNumber = parseInt($('#input2').val());
     var userSelect = $("input:radio[name=operator]:checked").val();
+    var output = 0;
 
       if (userSelect === 'add') {
-        calc2.addSum(firstNumber, secondNumber);
-        console.log(userSelect);
+        output = calc2.addSum(firstNumber, secondNumber);
+      } else if (userSelect === 'subtract') {
+        output = calc2.subtractSum(firstNumber, secondNumber);
+      } else if (userSelect === 'multiply') {
+        output = calc2.multiplySum(firstNumber, secondNumber);
+      } else {
+        output = calc2.divideSum(firstNumber, secondNumber);
       }
+
+      $('#output').text(output);
 
   });
 
